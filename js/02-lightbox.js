248e91f4213fc3b293be0a455b05f71a80655da9
Volodymyr Baraban <barabanv2003@gmail.com>
2022-03-23 00:04:11 +0200

    SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+//get galleryItems items for work
+const makeGalleryItemsLibrary = element => {
+  const { preview, original, description } = element;
+
+  return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" /> </a>`;
+};
+
+//set galleryItems on DOM
+const images = document.querySelector('.gallery');
+
+const makeSiteStructure = galleryItems.map(makeGalleryItemsLibrary).join('');
+
+images.innerHTML = makeSiteStructure;
+
+//Opening pictures
+images.addEventListener('click', e => {
+  e.preventDefault();
+});
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  close: true,
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: '250',
+});
